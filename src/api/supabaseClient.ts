304ea8f903daf15809a2c98c81766131e2da0a85
Monika Pazderova <pazderovamon@gmail.com>
2026-01-10 11:29:10 +0100

    Move database types to the model folder

--- src/api/supabaseClient.ts
+++ src/api/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-import type { Database } from '../types/database';
+import type { Database } from '../model/database';
 
 export const supabase = createClient<Database>(
   import.meta.env.VITE_SUPABASE_URL,
